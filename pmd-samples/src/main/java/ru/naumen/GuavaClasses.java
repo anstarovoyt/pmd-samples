ea25be587eb6f9df016cab8d301c6ae2ca40252d
Andrew Starovoyt <astarovoyt@naumen.ru>
2013-11-24 20:47:09 +0600

    fix

---
+++ pmd-samples/src/main/java/ru/naumen/GuavaClasses.java
@@ -0,0 +1,18 @@
+package ru.naumen;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+/**
+ * User: anstarovoyt
+ * Date: 11/24/13
+ * Time: 8:39 PM
+ */
+public class GuavaClasses
+{
+	public void init()
+	{
+		Lists.newArrayList();
+		Maps.newHashMap();
+	}
+}
