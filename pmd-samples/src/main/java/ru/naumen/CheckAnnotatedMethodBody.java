d472f333346dbc535ee0cf97a09ab1dcf643fb99
Andrew Starovoyt <astarovoyt@naumen.ru>
2013-11-24 20:29:16 +0600

    added new rule

---
+++ pmd-samples/src/main/java/ru/naumen/CheckAnnotatedMethodBody.java
@@ -0,0 +1,17 @@
+package ru.naumen;
+
+import org.junit.BeforeClass;
+
+/**
+ * User: anstarovoyt
+ * Date: 11/24/13
+ * Time: 7:32 PM
+ */
+public class CheckAnnotatedMethodBody
+{
+	@BeforeClass
+	public void init()
+	{
+		//System.out.println();
+	}
+}
